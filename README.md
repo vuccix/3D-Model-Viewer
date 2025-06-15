91526db923e232300feef608b2be2a3edf4dcb90
Vuk Djukanović <djukanovic.vuk@gmail.com>
2025-06-16 00:45:35 +0200

    added GLM and stb_image

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Playing-with-OpenGL
+# Playing with OpenGL
+
+todo
+
+## Dependencies
+
+- C++20 (or later)
+- CMake 3.20+
+- GLFW, GLAD
+- GLM
+- stb_image
+
+[//]: # (- assimp)
+[//]: # (- ImGui)
--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// Error callback
+// error callback
 void glfwErrorCallback(const int error, const char* description) {
     std::cerr << "GLFW Error [" << error << "]: " << description << '\n';
 }
 
 int main() {
-    // Init GLFW
+    // init GLFW
     glfwSetErrorCallback(glfwErrorCallback);
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
-        return -1;
+        return 1;
     }
 
-    // Request OpenGL 3.3 Core context
+    // request OpenGL 3.3 Core context
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+    // create window
+    GLFWwindow* window = glfwCreateWindow(1024, 576, "OpenGL Test", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
-        return -1;
+        return 1;
     }
 
     glfwMakeContextCurrent(window);
 
-    // Load OpenGL function pointers via GLAD
-    if (!gladLoadGL((GLADloadfunc) glfwGetProcAddress)) {
+    // load OpenGL function pointers via GLAD
+    if (!gladLoadGL(glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD\n";
-        return -1;
+        return 1;
     }
 
-    // Set viewport
-    glViewport(0, 0, 800, 600);
+    // set viewport
+    glViewport(0, 0, 1024, 576);
 
-    // Main loop
+    // main loop
     while (!glfwWindowShouldClose(window)) {
-        // Clear the screen with cornflower blue
         glClearColor(0.39f, 0.58f, 0.93f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -49,7 +48,7 @@ int main() {
         glfwPollEvents();
     }
 
-    // Clean up
+    // clean up
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
